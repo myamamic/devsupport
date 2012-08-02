9b98af0a04e3ec7d9fbf3723253fce4da952cd30
myamamic <myamaserow@gmail.com>
2012-08-02 15:09:47 +0900

    Add MyAlertDialogFragment class.
    [How to use]
    1. Implement MyAlertDialogFragment class.
    2. Override functions
        - onPositiveButtonClicked()
        - onNegativeButtonClicked()
    3. To create dialog, call MyAlertDialogFragment.newInstance() and show() method.

---
+++ src/myamamic/tp/devsupport/MyAlertDialogFragment.java
@@ -0,0 +1,66 @@
+package myamamic.tp.devsupport;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MyAlertDialogFragment extends DialogFragment {
+
+    public interface OnButtonClickListener {
+        public void onPositiveButtonClicked();
+        public void onNegativeButtonClicked();
+    }
+
+    private OnButtonClickListener mOnButtonClickListener;
+
+    public static MyAlertDialogFragment newInstance(int title) {
+        MyAlertDialogFragment frag = new MyAlertDialogFragment();
+        Bundle args = new Bundle();
+        args.putInt("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+ 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (activity instanceof OnButtonClickListener == false) {
+            throw new ClassCastException("The activity does not implement OnButtonClickListener.");
+        }
+
+        mOnButtonClickListener = (OnButtonClickListener)activity;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        int title = getArguments().getInt("title");
+
+        return new AlertDialog.Builder(getActivity())
+        .setIcon(android.R.drawable.ic_dialog_alert)
+        .setTitle(title)
+        .setPositiveButton(android.R.string.ok,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    if (mOnButtonClickListener != null) {
+                        mOnButtonClickListener.onPositiveButtonClicked();
+                    }
+                }
+            }
+        )
+        .setNegativeButton(android.R.string.cancel,
+            new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    if (mOnButtonClickListener != null) {
+                        mOnButtonClickListener.onNegativeButtonClicked();
+                    }
+                }
+            }
+        )
+        .create();
+    }
+}
