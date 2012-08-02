b5eed8f8cde2d3872866fe257cffd95cb7e8022b
myamamic <myamaserow@gmail.com>
2012-08-02 15:26:47 +0900

    Create BaseTestActivity class

---
+++ src/myamamic/tp/devsupport/BaseTestActivity.java
@@ -0,0 +1,33 @@
+package myamamic.tp.devsupport;
+
+
+import android.app.Activity;
+import android.view.KeyEvent;
+
+public class BaseTestActivity extends Activity 
+        implements MyAlertDialogFragment.OnButtonClickListener {
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showConfirmFinishDialog();
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void showConfirmFinishDialog() {
+        MyAlertDialogFragment newFragment = MyAlertDialogFragment.newInstance(R.string.confirm_finish);
+        newFragment.show(getFragmentManager(), "dialog");
+    }
+
+    @Override
+    public void onPositiveButtonClicked() {
+        finish();
+    }
+
+    @Override
+    public void onNegativeButtonClicked() {
+        // Cancel finish
+    }
+}
